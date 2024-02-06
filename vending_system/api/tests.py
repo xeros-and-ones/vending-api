d30edecbf90e92e91ea88b1f6060dfdc8a48811e
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-02-06 22:35:29 +0200

    init testCases

--- vending_system/api/tests.py
+++ vending_system/api/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import response
+from django.test import TestCase, Client
+from rest_framework import status
+from .models import Product, User
 
-# Create your tests here.
+
+class UserCrudTest(TestCase):
+    pass
+
+
+class ProductCrudTest(TestCase):
+    pass
+
+
+class BuyerOperationsTest(TestCase):
+    pass
