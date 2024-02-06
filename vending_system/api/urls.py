60134d8017b35a9c5ed212067498cf23df458b3e
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-02-06 22:35:13 +0200

    Chore: cleanup

--- vending_system/api/urls.py
+++ vending_system/api/urls.py
@@ -14,9 +14,6 @@ from .views import (
 )
 
 urlpatterns = [
-    path("deposit/", Deposit.as_view()),
-    path("reset", ResetDeposit.as_view()),
-    path("buy", Buy.as_view()),
     path("users/create", CreateUser.as_view()),
     path("users/view", ViewUsers.as_view()),
     path("users/delete", DeleteUsers.as_view()),
@@ -25,4 +22,7 @@ urlpatterns = [
     path("products/view", ProductList.as_view()),
     path("products/<int:product_id>/delete", ProductDelete.as_view()),
     path("products/<int:product_id>/update", ProductUpdate.as_view()),
+    path("deposit/", Deposit.as_view()),
+    path("reset", ResetDeposit.as_view()),
+    path("buy", Buy.as_view()),
 ]
