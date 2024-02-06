ff4b9d5bb66368a8f9b3fc76b9433ca2c0e26ead
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-02-06 14:42:08 +0200

    feat: add endpints

--- vending_system/api/urls.py
+++ vending_system/api/urls.py
@@ -1,8 +1,28 @@
 from django.urls import path
-from .views import Deposit, CreateUser, ResetDeposit
+from .views import (
+    CreateProduct,
+    ProductDelete,
+    ProductList,
+    DeleteUsers,
+    Deposit,
+    Buy,
+    CreateUser,
+    ProductUpdate,
+    ResetDeposit,
+    UpdateUsers,
+    ViewUsers,
+)
 
 urlpatterns = [
     path("deposit/", Deposit.as_view()),
-    path("create", CreateUser.as_view()),
     path("reset", ResetDeposit.as_view()),
+    path("buy", Buy.as_view()),
+    path("users/create", CreateUser.as_view()),
+    path("users/view", ViewUsers.as_view()),
+    path("users/delete", DeleteUsers.as_view()),
+    path("users/update", UpdateUsers.as_view()),
+    path("products/create", CreateProduct.as_view()),
+    path("products/view", ProductList.as_view()),
+    path("products/<int:product_id>/delete", ProductDelete.as_view()),
+    path("products/<int:product_id>/update", ProductUpdate.as_view()),
 ]
